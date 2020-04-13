09fd6a6553ebb471af59c9c6ff9f8d454f68daa9
Artem Bayandin <bayandin.artem.official@gmail.com>
2020-04-13 22:59:19 +0300

    dummy test added for Azure DevOps course on Udemy

---
+++ clean-architecture-dotnetcore-api/tests/Domain.Tests/SampleTestsForAzureDevOps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestsBase;
+using Xunit;
+
+namespace Domain.Tests
+{
+    public class SampleTestsForAzureDevOps : UnitTestsBase
+    {
+        public SampleTestsForAzureDevOps()
+        {
+        }
+
+        [Fact]
+        public async Task RunSampleTest()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            Assert.True(true);
+        }
+    }
+}
